6917fd8370871112b93fc03a47c4b28999906b5d
Freddy Monroy <fredydeltoro@gmail.com>
2023-10-22 15:11:12 -0600

    JWT setup

--- todo/api/urls.py
+++ todo/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt import views as jwt_views
 from .views  import UserView, GroupView
 
 router = routers.DefaultRouter()
@@ -10,4 +11,5 @@ router.register(r'groups', GroupView)
 
 urlpatterns = [
   path('', include(router.urls)),
+  path('login', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair')
 ]
