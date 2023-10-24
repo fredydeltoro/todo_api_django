d2cba7937b76b3466b33b61deefcc9d15e558de3
Freddy Monroy <fredydeltoro@gmail.com>
2023-10-24 17:06:54 -0600

    Todo model

--- todo/api/models.py
+++ todo/api/models.py
@@ -6,11 +6,23 @@ User = get_user_model()
 
 # Create your models here.
 class TodoList(models.Model):
-  name=models.CharField(max_length=100)
+  name = models.CharField(max_length=100, blank=False, null=False)
   description = models.TextField()
   user = models.ForeignKey(User, on_delete=models.CASCADE)
   created_at = AutoCreatedField()
   updated_at = AutoLastModifiedField()
   
   def __str__(self):
-        return self.name
+    return self.name
+      
+
+class Todo(models.Model):
+  name = models.CharField(max_length=100, blank=False, null=False)
+  description = models.TextField()
+  status = models.BooleanField(default=False)
+  todo_list = models.ForeignKey(TodoList, on_delete=models.CASCADE)
+  created_at = AutoCreatedField()
+  updated_at = AutoLastModifiedField()
+  
+  def __str__(self):
+    return self.name
